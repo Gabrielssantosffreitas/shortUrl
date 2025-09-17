6bf62ff3522f78fe46837f9efb9694ba31374c68
Gabriel Santos <gabrielsantos022freitas@gmail.com>
2025-09-17 17:05:27 -0300

    add verificacao de Ativo or Desativo end add sum click

--- src/main/java/com/gabriel/EncurtadorDelinkSimples/services/RedirectService.java
+++ src/main/java/com/gabriel/EncurtadorDelinkSimples/services/RedirectService.java
@@ -1,6 +1,7 @@
 package com.gabriel.EncurtadorDelinkSimples.services;
 
 import com.gabriel.EncurtadorDelinkSimples.Entitys.LinkEntity;
+import com.gabriel.EncurtadorDelinkSimples.enums.EstateAtivoDesetivado;
 import com.gabriel.EncurtadorDelinkSimples.repositorys.LinksRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +23,14 @@ public class RedirectService {
                 repository.delete(l);
                 throw new RuntimeException("esse link Ja expirou");
             }
+            if (l.getEstate() == EstateAtivoDesetivado.DESATIVO){
+                throw new RuntimeException("Esse link nao esta ativo");
+            }
             return l;
         }).orElseThrow(RuntimeException::new);
 
+        linkEntity.setClick(linkEntity.getClick()+1);
+        repository.save(linkEntity);
          return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).header(
                  "Location",
                  linkEntity.getUrl()
